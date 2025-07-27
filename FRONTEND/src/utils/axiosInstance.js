33a39199f5232a267a2ffc51fa08aa155eb1358f
PrathamPeriwal <prathamperiwal5@gmail.com>
2025-07-27 16:05:04 +0530

    Update axiosInstance.js

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = "https://linkshrink-nsmw.onrender.com";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
